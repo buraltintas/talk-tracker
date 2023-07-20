8f3751253d14dd66debdb01e8766d3524ac976aa
buraltintas <burak.altintas@yahoo.com.tr>
2023-07-20 16:16:41 +0300

    delete compnent tests

--- src/pages/mainPage.cy.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import MainPage from './main-page'
-
-describe('<MainPage />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<MainPage />)
-  })
-})
--- src/pages/meeting/[id]Meeting.cy.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Meeting from './[id]'
-
-describe('<Meeting />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<Meeting />)
-  })
-})
